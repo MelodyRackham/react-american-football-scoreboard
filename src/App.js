e434537700c0ccb6f267abdeecd55b611ce37ec4
Mel Maki <melody.rackham@gmail.com>
2019-10-07 14:53:00 -0600

    Completed project and one stretch goal

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import BottomRow from './BottomRow';
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [Lions, setCountLions] = useState(10);
-  const [Tigers, setCountTigers] = useState(40);
+  const [Lions, setCountLions] = useState(0);
+  const [Tigers, setCountTigers] = useState(0);
+
   return (
     <div className='container'>
       <section className='scoreboard'>
@@ -29,12 +30,20 @@ function App() {
       <section className='buttons'>
         <div className='homeButtons'>
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className='homeButtons__touchdown'>Home Touchdown</button>
-          <button className='homeButtons__fieldGoal'>Home Field Goal</button>
+          <button className='homeButtons__touchdown' onClick={() => setCountLions(Lions + 7)}>
+            Home Touchdown
+          </button>
+          <button className='homeButtons__fieldGoal' onClick={() => setCountLions(Lions + 3)}>
+            Home Field Goal
+          </button>
         </div>
         <div className='awayButtons'>
-          <button className='awayButtons__touchdown'>Away Touchdown</button>
-          <button className='awayButtons__fieldGoal'>Away Field Goal</button>
+          <button className='awayButtons__touchdown' onClick={() => setCountTigers(Tigers + 7)}>
+            Away Touchdown
+          </button>
+          <button className='awayButtons__fieldGoal' onClick={() => setCountTigers(Tigers + 3)}>
+            Away Field Goal
+          </button>
         </div>
       </section>
     </div>
